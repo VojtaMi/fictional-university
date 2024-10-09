26602545f986d7c8e61d798fd98d135eefc06d69
vojta_mi <tami.vojkel@gmail.com>
2024-10-09 15:12:26 +0200

    bg image php function

--- functions.php
+++ functions.php
@@ -8,3 +8,8 @@ function university_files()
     wp_enqueue_style("google-fonts", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
 }
 add_action("wp_enqueue_scripts", "university_files");
+
+// Backgroun Image
+function bg_image($file_path) {
+    echo 'style="background-image: url(' . get_theme_file_uri($file_path) . ')"';
+}
--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 get_header();
 ?>
 <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo
-                                                                get_theme_file_uri("images/library-hero.jpg");
-                                                                ?>)"></div>
+    <div class="page-banner__bg-image" <?php bg_image("images/library-hero.jpg");?>></div>
     <div class="page-banner__content container t-center c-white">
         <h1 class="headline headline--large">Welcome!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
@@ -75,9 +73,7 @@ get_header();
 <div class="hero-slider">
     <div data-glide-el="track" class="glide__track">
         <div class="glide__slides">
-            <div class="hero-slider__slide" style="background-image: url(<?php echo
-                                                                get_theme_file_uri("images/bus.jpg");
-                                                                ?>)">
+            <div class="hero-slider__slide" <?php bg_image("images/bus.jpg");?>">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -86,9 +82,7 @@ get_header();
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide" style="background-image: url(<?php echo
-                                                                get_theme_file_uri("images/apples.jpg");
-                                                                ?>)">
+            <div class="hero-slider__slide" <?php bg_image("images/apples.jpg");?>">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -97,9 +91,7 @@ get_header();
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide" style="background-image: url(<?php echo
-                                                                get_theme_file_uri("images/bread.jpg");
-                                                                ?>)">
+            <div class="hero-slider__slide" <?php bg_image("images/bread.jpg");?>">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Food</h2>
