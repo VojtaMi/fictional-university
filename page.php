45b33f25a269c501ce3ed70ed9aea9800c459549
vojta_mi <tami.vojkel@gmail.com>
2024-10-09 19:04:30 +0200

    bg image to internal page

--- page.php
+++ page.php
@@ -4,7 +4,7 @@ while (have_posts()) {
     the_post();
 ?>
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
+        <div class="page-banner__bg-image" <?php bg_image("images/ocean.jpg") ?>></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title() ?></h1>
             <div class="page-banner__intro">
