3fde6ba4a433c96cdaff85c9d4c542de41a2797d
vojta_mi <tami.vojkel@gmail.com>
2024-10-15 13:57:25 +0200

    using custom query to retrieve recent post data for the front page

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 <div class="page-banner">
-    <div class="page-banner__bg-image" <?php bg_image("images/library-hero.jpg");?>></div>
+    <div class="page-banner__bg-image" <?php bg_image("images/library-hero.jpg"); ?>></div>
     <div class="page-banner__content container t-center c-white">
         <h1 class="headline headline--large">Welcome!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
@@ -44,26 +44,32 @@ get_header();
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
 
-            <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="#">
-                    <span class="event-summary__month">Jan</span>
-                    <span class="event-summary__day">20</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
-                    <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
+            <?php $recent_posts = new WP_Query([
+                'posts_per_page' => 2
+            ]);
+
+            while ($recent_posts->have_posts()) {
+                $recent_posts->the_post();
+                ?>
+                <div class="event-summary">
+                    <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>"> 
+                        <span class="event-summary__month"><?php echo get_the_date('M'); ?></span> 
+                        <span class="event-summary__day"><?php echo get_the_date('d'); ?></span>
+                    </a>
+                    <div class="event-summary__content"> 
+                        <h5 class="event-summary__title headline headline--tiny">
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        </h5>
+                        <p><?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                    </div>
+                    
                 </div>
-            </div>
-            <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="#">
-                    <span class="event-summary__month">Feb</span>
-                    <span class="event-summary__day">04</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
-                    <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-                </div>
-            </div>
+                <?php
+            }
+            
+            wp_reset_postdata();
+
+            ?>
 
             <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
@@ -73,7 +79,7 @@ get_header();
 <div class="hero-slider">
     <div data-glide-el="track" class="glide__track">
         <div class="glide__slides">
-            <div class="hero-slider__slide" <?php bg_image("images/bus.jpg");?>>
+            <div class="hero-slider__slide" <?php bg_image("images/bus.jpg"); ?>>
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -82,7 +88,7 @@ get_header();
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide" <?php bg_image("images/apples.jpg");?>>
+            <div class="hero-slider__slide" <?php bg_image("images/apples.jpg"); ?>>
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -91,7 +97,7 @@ get_header();
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide" <?php bg_image("images/bread.jpg");?>>
+            <div class="hero-slider__slide" <?php bg_image("images/bread.jpg"); ?>>
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Food</h2>
